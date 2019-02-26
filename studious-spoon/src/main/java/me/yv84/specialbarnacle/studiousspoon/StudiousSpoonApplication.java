a177593e2edf8600ed2ba7d7210d16be348dcf19
Vladimir Yudintsev <yv87@ya.ru>
2019-02-26 19:09:40 +0500

    spring boot init studious-spoon service

---
+++ studious-spoon/src/main/java/me/yv84/specialbarnacle/studiousspoon/StudiousSpoonApplication.java
@@ -0,0 +1,13 @@
+package me.yv84.specialbarnacle.studiousspoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudiousSpoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudiousSpoonApplication.class, args);
+	}
+
+}
---
+++ studious-spoon/src/test/java/me/yv84/specialbarnacle/studiousspoon/StudiousSpoonApplicationTests.java
@@ -0,0 +1,16 @@
+package me.yv84.specialbarnacle.studiousspoon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StudiousSpoonApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
