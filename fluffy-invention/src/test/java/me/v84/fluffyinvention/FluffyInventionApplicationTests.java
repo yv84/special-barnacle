caae8d3b19c7ffa72ee3755965b985c37f6c0757
Vladimir Yudintsev <yv87@ya.ru>
2019-03-04 14:33:32 +0500

    fluffy-invention init service

---
+++ fluffy-invention/src/test/java/me/v84/fluffyinvention/FluffyInventionApplicationTests.java
@@ -0,0 +1,16 @@
+package me.v84.fluffyinvention;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FluffyInventionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
