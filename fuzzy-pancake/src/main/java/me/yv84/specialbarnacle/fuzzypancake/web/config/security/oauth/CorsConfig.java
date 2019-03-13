273bb8fa1e7dff027c47b93e373e1c384f1c21d9
Vladimir Yudintsev <yv87@ya.ru>
2019-03-13 09:45:12 +0500

    cors enable spring boot 2

---
+++ fuzzy-pancake/src/main/java/me/yv84/specialbarnacle/fuzzypancake/web/config/security/oauth/CorsConfig.java
@@ -0,0 +1,23 @@
+package me.yv84.specialbarnacle.fuzzypancake.web.config.security.oauth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+	    	.allowCredentials(true)
+	    	.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTION")
+	    	.allowedOrigins("*")
+	    	.allowedHeaders("*")
+	    	.maxAge(1000);
+    }
+
+}
